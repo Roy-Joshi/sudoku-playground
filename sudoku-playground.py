b834466b60bb64f154f0794f1258b37e870a879f
Roy Joshi <royjoshi@protonmail.com>
2022-05-14 13:16:05 -0700

    Added execution timer and reformatted printsudoku import

--- sudoku-playground.py
+++ sudoku-playground.py
@@ -1,6 +1,12 @@
 from sudoku_file_input import SudokuFileInput
-import print_sudoku
+from print_sudoku import PrintSudoku
+import time
+
+
+startTime = time.time()
 
 puzzles = SudokuFileInput(10)
+PrintSudoku.printSudoku(puzzles.sudoku_puzzles[0])
 
-print_sudoku.PrintSudoku.printSudoku(puzzles.sudoku_puzzles[0])
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
