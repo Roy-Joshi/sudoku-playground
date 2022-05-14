b53e1d49c890d6d7b03006781cd9b6016c18c745
Roy Joshi <royjoshi@protonmail.com>
2022-05-13 22:38:35 -0700

    Print sudoku function

---
+++ print_sudoku.py
@@ -0,0 +1,17 @@
+class PrintSudoku:
+
+    def __init__(self):
+        return
+
+    def print(puzzle):
+        print()
+        for i in range(len(puzzle)):
+            print(puzzle[i], end =" ")
+            if (i + 1) % 9 == 0:
+                print()
+            elif (i + 1) % 3 == 0:
+                print ("|", end=' ')
+
+            if (i + 1) % 27 == 0:
+                print ("---------------------")
+        print()
