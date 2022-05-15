79c6d695f53c31e291a5e294caed13e96d238ebf
Roy Joshi <royjoshi@protonmail.com>
2022-05-15 12:48:34 -0700

    Reformatted print sudoku to add rows and columns

--- print_sudoku.py
+++ print_sudoku.py
@@ -4,14 +4,17 @@ class PrintSudoku:
         return
 
     def printSudoku(puzzle):
-        print()
+        print("    1 2 3   4 5 6   7 8 9")
+        print("    ---------------------")
         for i in range(len(puzzle)):
+            if i % 9 == 0:
+                print(int(i / 9) + 1, end=' | ')
             print(puzzle[i], end =" ")
             if (i + 1) % 9 == 0:
                 print()
             elif (i + 1) % 3 == 0:
                 print ("|", end=' ')
-
             if (i + 1) % 27 == 0:
-                print ("---------------------")
+                print ("    ---------------------")
+
         print()
